43ea2ed85b1fda607be87be4075d9106496d82c6
betrakiss <angelin.nedelchev@outlook.com>
2016-10-30 12:54:12 +0200

    Add Link class.

---
+++ src/VacationReservations/VacationReservations/Common/Link.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VacationReservations.Common
+{
+    public class Link
+    {
+        private static string BuildAbsolute(string relativeUri)
+        {
+            // get current uri
+            Uri uri = HttpContext.Current.Request.Url;
+            // build absolute path
+            string app = HttpContext.Current.Request.ApplicationPath;
+            if (!app.EndsWith("/")) app += "/";
+            relativeUri = relativeUri.TrimStart('/');
+            // return the absolute path
+            return HttpUtility.UrlPathEncode(
+            String.Format("http://{0}:{1}{2}{3}",
+            uri.Host, uri.Port, app, relativeUri));
+        }
+        // Generate a department URL
+        public static string ToDepartment(string departmentId, string page)
+        {
+            if (page == "1")
+                return BuildAbsolute(String.Format("Catalog.aspx?DepartmentID={0}", departmentId));
+
+            return BuildAbsolute(String.Format("Catalog.aspx?DepartmentID={0}&Page {1}", departmentId, page));
+        }
+        // Generate a department URL for the first page
+        public static string ToDepartment(string departmentId)
+        {
+            return ToDepartment(departmentId, "1");
+        }
+    }
+}
