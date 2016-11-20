1b8247b1e6c47989814b160448a3f5cb7ad9b1e2
betrakiss <angelin.nedelchev@outlook.com>
2016-11-20 15:19:34 +0200

    Fix url making regex not matching cyrilic characters.

--- src/VacationReservations/VacationReservations/Common/Link.cs
+++ src/VacationReservations/VacationReservations/Common/Link.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using VacationReservations.DataAccess;
 
@@ -40,7 +41,12 @@ namespace VacationReservations.Common
     }
 
     public class Link
-    {
+    { // regular expression that removes characters that aren't a-z, 0-9, dash,
+      // underscore or space
+        private static Regex purifyUrlRegex = new Regex("[^-a-zA-ZА-Яа-я0-9_ ]", RegexOptions.Compiled);
+        // regular expression that changes dashes, underscores and spaces to dashes
+        private static Regex dashesRegex = new Regex("[-_ ]+", RegexOptions.Compiled);
+
         private static string BuildAbsolute(string relativeUri)
         {
             // get current uri
@@ -54,14 +60,19 @@ namespace VacationReservations.Common
             String.Format("http://{0}:{1}{2}{3}",
             uri.Host, uri.Port, app, relativeUri));
         }
+
         // Generate a department URL
         public static string ToDepartment(string departmentId, string page)
         {
+            // prepare department URL name
+            DepartmentDetails d = CatalogAccess.GetDepartmentDetails(departmentId);
+            string deptUrlName = PrepareUrlText(d.Name);
             if (page == "1")
-                return BuildAbsolute(String.Format("Catalog.aspx?DepartmentID={0}", departmentId));
-
-            return BuildAbsolute(String.Format("Catalog.aspx?DepartmentID={0}&Page={1}", departmentId, page));
+                return BuildAbsolute(String.Format("{0}-d{1}/", deptUrlName, departmentId));
+            else
+                return BuildAbsolute(String.Format("{0}-d{1}/Page={2}", deptUrlName, departmentId, page));
         }
+
         // Generate a department URL for the first page
         public static string ToDepartment(string departmentId)
         {
@@ -150,17 +161,18 @@ namespace VacationReservations.Common
             return GenericDataAccess.ExecuteSelectCommand(comm);
         }
 
-        public static string ToCategory(string departmentId, string categoryId, string
-page)
+        public static string ToCategory(string departmentId, string categoryId, string page)
         {
+            // prepare department and category URL names
+            DepartmentDetails d = CatalogAccess.GetDepartmentDetails(departmentId);
+            string deptUrlName = PrepareUrlText(d.Name);
+            CategoryDetails c = CatalogAccess.GetCategoryDetails(categoryId);
+            string catUrlName = PrepareUrlText(c.Name);
+            // build category URL
             if (page == "1")
-                return BuildAbsolute(String.Format(
-               "Catalog.aspx?DepartmentID={0}&CategoryID={1}",
-               departmentId, categoryId));
+                return BuildAbsolute(String.Format("{0}-d{1}/{2}-c{3}/", deptUrlName, departmentId, catUrlName, categoryId));
             else
-                return BuildAbsolute(String.Format(
-               "Catalog.aspx?DepartmentID={0}&CategoryID={1}&Page={2}",
-               departmentId, categoryId, page));
+                return BuildAbsolute(String.Format("{0}-d{1}/{2}-c{3}/Page-{4}/", deptUrlName, departmentId, catUrlName, categoryId, page));
         }
         public static string ToCategory(string departmentId, string categoryId)
         {
@@ -168,12 +180,29 @@ page)
         }
         public static string ToProduct(string productId)
         {
-            return BuildAbsolute(String.Format("Product.aspx?ProductID={0}", productId));
+            // prepare product URL name
+            ProductDetails p = CatalogAccess.GetProductDetails(productId.ToString());
+            string prodUrlName = PrepareUrlText(p.Name);
+            // build product URL
+            return BuildAbsolute(String.Format("{0}-p{1}/", prodUrlName, productId));
         }
         public static string ToProductImage(string fileName)
         {
             // build product URL
             return BuildAbsolute("/ProductImages/" + fileName);
+        }
+
+        // prepares a string to be included in an URL
+        private static string PrepareUrlText(string urlText)
+        {
+            // remove all characters that aren't a-z, 0-9, dash, underscore or space
+            urlText = purifyUrlRegex.Replace(urlText, "");
+            // remove all leading and trailing spaces
+            urlText = urlText.Trim();
+            // change all dashes, underscores and spaces to dashes
+            urlText = dashesRegex.Replace(urlText, "-");
+            // return the modified string
+            return urlText;
         }
     }
 }
