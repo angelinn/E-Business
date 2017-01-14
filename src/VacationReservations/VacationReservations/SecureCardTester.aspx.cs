a03918adfac2a87e011a738c53e9a69da97d877b
betrakiss <angelin.nedelchev@outlook.com>
2017-01-14 21:41:36 +0200

    Add Secure Card. Add Register web form.

---
+++ src/VacationReservations/VacationReservations/SecureCardTester.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VacationReservations.Common;
+
+namespace VacationReservations
+{
+    public partial class SecureCardTester : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void processButton_Click(object sender, EventArgs e)
+        {
+            SecureCard encryptedCard =
+            new SecureCard(cardHolderBox.Text, cardNumberBox.Text,
+            issueDateBox.Text, expiryDateBox.Text, issueNumberBox.Text,
+            cardTypeBox.Text);
+            string encryptedData = encryptedCard.EncryptedData;
+            SecureCard decryptedCard = new SecureCard(encryptedData);
+            string decryptedData = string.Format(
+            "{0}, {1}, {2}, {3}, {4}, {5}",
+            decryptedCard.CardHolder, decryptedCard.CardNumber,
+            decryptedCard.IssueDate, decryptedCard.ExpiryDate,
+            decryptedCard.IssueNumber, decryptedCard.CardType);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Encrypted data:<br />");
+            sb.Append("<textarea style=\"width:400px;height:150px;\">");
+            sb.Append(encryptedData);
+            sb.Append("</textarea><br />Decrypted data:");
+            sb.Append(decryptedData);
+            result.Text = sb.ToString();
+        }
+    }
+}
