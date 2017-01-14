bab979faaebac2de4766af9d4eb53c949ee1d6eb
betrakiss <angelin.nedelchev@outlook.com>
2017-01-14 21:30:41 +0200

    Add String Encryptor.

---
+++ src/VacationReservations/VacationReservations/StringEncryptorTester.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/VacationReservationsPage.Master" AutoEventWireup="true" CodeBehind="StringEncryptorTester.aspx.cs" Inherits="VacationReservations.StringEncryptorTester" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    Enter data to encrypt:
+ <br />
+    <asp:TextBox ID="encryptBox" runat="server" />
+    <br />
+    Enter data to decrypt:
+ <br />
+    <asp:TextBox ID="decryptBox" runat="server" />
+    <br />
+    <asp:Button ID="processButton" runat="server" Text="Process" OnClick="processButton_Click" />
+    <br />
+    <asp:Label ID="result" runat="server" />
+</asp:Content>
---
+++ src/VacationReservations/VacationReservations/StringEncryptorTester.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VacationReservations.Common;
+
+namespace VacationReservations
+{
+    public partial class StringEncryptorTester : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void processButton_Click(object sender, EventArgs e)
+        {
+            string stringToEncrypt = encryptBox.Text;
+            string stringToDecrypt = decryptBox.Text;
+            string encryptedString =
+            StringEncryptor.Encrypt(stringToEncrypt);
+            if (stringToDecrypt == "")
+            {
+                stringToDecrypt = encryptedString;
+            }
+            string decryptedString =
+            StringEncryptor.Decrypt(stringToDecrypt);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Encrypted data: ");
+            sb.Append(encryptedString);
+            sb.Append("<br />Decrypted data: ");
+            sb.Append(decryptedString);
+            result.Text = sb.ToString();
+        }
+    }
+}
